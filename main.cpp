2736769a7dd6e946b4e490a1a96970d438875b29
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-03-27 07:00:37 +1030

    completed

--- main.cpp
+++ main.cpp
@@ -61,6 +61,48 @@ std::string sub(std::string I1, std::string I2, int base) {
     return res;
 }
 
-int main() {
+std::string karatsuba(std::string I1, std::string I2, int base) {
+    if (I1.length() == 1 && I2.length() == 1) {
+        int product = (I1[0] - '0') * (I2[0] - '0');
+        return std::to_string(product);
+    }
 
+    if (I1.length() < I2.length()) {
+        return karatsuba(I2, I1, base);
+    }
+
+    for (int i = 0; i < I1.length() - I2.length(); i++) {
+        I2 = "0" + I2;
+    }
+
+    int n = I1.length();
+    int half = n/2;
+
+    std::string a1 = I1.substr(0, half);
+    std::string a0 = I1.substr(half);
+    std::string b1 = I2.substr(0, half);
+    std::string b0 = I2.substr(half);
+
+    std::string p2 = karatsuba(a1, b1, base);
+    std::string p1 = karatsuba(add(a1, a0, base), add(b1, b0, base), base);
+    std::string p0 = karatsuba(a0, b0, base);
+
+    std::string p3 = sub(p1, add(p2, p0, base), base);
+
+    for (int i = 0; i < half; i++) {
+        p2 += "00";
+        p3 += "0";
+    }
+
+    std::string res = add(add(p2, p3, base), p0, base);
+
+    return res;
+}
+
+int main() {
+    std::string I1, I2;
+    int base;
+
+    std::cin >> I1 >> I2 >> base;
+    std::cout << add(I1, I2, base) << " " << karatsuba(I1, I2, base) << " " << 0;
 }
